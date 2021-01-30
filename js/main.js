1461b7282aed1bf7fbd049a3c7194eb31a337ad4
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-30 19:09:42 +1000

    Removed Dropdown Functionality

--- js/main.js
+++ js/main.js
@@ -8,23 +8,3 @@ document.getElementById("menu-icon").addEventListener('click', () => {
 document.getElementById("close-icon").addEventListener('click', () => {
     document.querySelector('.mobile-menu').style.display = "none";            
 });
-
-// Dropdown Menu Animation
-const portfolioNavItem = document.querySelector('.portfolio-nav-item');
-const dropdown = document.getElementById('dropdown');
-
-portfolioNavItem.addEventListener('mouseenter', () => {
-    
-    if (dropdown.style.display != 'flex') {
-        dropdown.style.display = 'flex';
-        gsap.to('#dropdown', { opacity: 1, duration: 1 });
-    }
-});
-
-portfolioNavItem.addEventListener('mouseleave', () => {
-
-    if (dropdown.style.display !== 'none') {
-        gsap.to('#dropdown', { opacity: 0, duration: 1 });
-        setTimeout(() => { dropdown.style.display = 'none' }, 1000);
-    }
-})
